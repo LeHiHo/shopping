f0da884dacd72df1be78620510bf216cc93468d5
Hojin <wkdghwls0623@gmail.com>
2024-05-01 16:43:41 +0900

    Env: 초기셋팅

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: '쿠알테',
+  description: "쿠팡,알리,테무 가격비교 사이트",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
